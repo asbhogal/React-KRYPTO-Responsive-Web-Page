1804526b494441cfe13f0e50fa3a1b9fc26e0c66
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-22 23:07:36 +0000

    added testimonial data

---
+++ src/js/testimonialData.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        key: 1,
+        img: '1.png',
+        description: 'lorem ipsum dolor sit amel consectetur adipiscing elit. nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel.',
+        name: 'Olivia Cole'
+    },
+    {
+        key: 2,
+        img: '2.png',
+        description: 'lorem ipsum dolor sit amel consectetur adipiscing elit. nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel.',
+        name: 'Evan White'
+    },
+    {
+        key: 3,
+        img: '3.png',
+        description: 'lorem ipsum dolor sit amel consectetur adipiscing elit. nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel.',
+        name: 'Jessica Page'
+    }
+]
