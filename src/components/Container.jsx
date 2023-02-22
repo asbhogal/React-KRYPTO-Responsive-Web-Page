3683ae91776a5565f2893bc8bdf2644d6535fbf9
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-22 13:19:33 +0000

    added optimized assets and additional font

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,25 @@
+import leftHeroImg from "../assets/images/HeroImg";
+
 const Container = () => {
     return (
         <main className="MainContainer">
-            <section className="HeroSection"></section>
+            <section className="HeroSection">
+                <div className="TopContent">
+                    <div className="LeftSideContent">
+                        <h1 className="SiteHeadingOne">discover and collect rate NFTs</h1>
+                        <p className="SiteHeroParagraph">the most secure marketplace for buying and selling unique crypto assets</p>
+                        <button className="CTAButton">BUY NFTS</button>
+                        <button className="SellNFTsCTAButton">SELL NFTS</button>
+                    </div>
+                    <div className="RightSideContent"></div>
+                </div>
+                <div className="FeaturedOnContent">
+                    <p className="FeaturedOnTitle">Featured on</p>
+                    <div className="FeaturedOnLogos">
+
+                    </div>
+                </div>
+            </section>
             <section className="AnalyticsSection"></section>
             <section className="AppSection"></section>
             <section className="SellNFTSection"></section>
