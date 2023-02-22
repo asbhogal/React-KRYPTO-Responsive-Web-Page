a672690e94b94e356f5d816d2b60acadd2a13654
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-22 12:44:02 +0000

    started content styling and responsive for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,22 @@
+import brandLogo from "../assets/logos/krypto.svg";
+
 const Header = () => {
     return (
-        <header></header>
+        <header className="Header">
+            <img src={ brandLogo }></img>
+            <nav className="HeaderNav">
+                <ul className="HeaderNavList">
+                    <li className="HeaderNavListItem">about</li>
+                    <li className="HeaderNavListItem">pricing</li>
+                    <li className="HeaderNavListItem">contact</li>
+                </ul>
+                <button className="CTAButton">
+                    <span className="AnimatedPulseFront"></span>
+                    <span className="AnimatedPulseBack"></span>
+                    <p className="CTAName">buy nfts</p>
+                </button>
+            </nav>
+        </header>
     )
 };
 
