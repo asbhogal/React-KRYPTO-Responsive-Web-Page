203e05bfc2d564b4b94c5fdcee8e12153ad467c8
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-23 14:37:21 +0000

    create responsive neader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import brandLogo from "../assets/logos/krypto.svg";
 
 const Header = () => {
+
+    const   [active, setActive] = useState("HeaderNavList"),
+            [toggle, setToggle] = useState("NavbarToggle");
+
+    const   navToggle = () => {
+        active === "HeaderNavList" 
+        ? setActive("HeaderNavList NavActive") 
+        : setActive("HeaderNavList");
+
+        toggle === "NavbarToggle" 
+        ? setToggle("NavbarToggle ToggleActive") 
+        : setToggle("NavbarToggle");
+    }
+
     return (
         <header className="Header">
             <img src={ brandLogo }></img>
             <nav className="HeaderNav">
-                <ul className="HeaderNavList">
+                <ul className={ active }>
                     <li className="HeaderNavListItem">about</li>
                     <li className="HeaderNavListItem">pricing</li>
                     <li className="HeaderNavListItem">contact</li>
                 </ul>
-                <button className="CTAButton">
-                    <span className="AnimatedPulseFront"></span>
-                    <span className="AnimatedPulseBack"></span>
-                    <p className="CTAName">buy nfts</p>
+
+                <button className={ toggle } onClick={ navToggle }>
+                    <span className="ToggleLine"></span>
+                    <span className="ToggleLine"></span>
+                    <span className="ToggleLine"></span>
                 </button>
             </nav>
+            <button className="CTAButton">
+                <span className="AnimatedPulseFront"></span>
+                <span className="AnimatedPulseBack"></span>
+                <p className="CTAName">buy nfts</p>
+            </button>
         </header>
     )
 };
