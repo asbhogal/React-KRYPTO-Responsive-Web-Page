79bf887df38ee8c87c096be5bf906c0229bad18a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-23 12:23:44 +0000

    ran build-prod for deployment - started content and styling for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,39 @@
+import kryptoLogo from '../assets/logos/krypto.svg';
+
 const Footer = () => {
     return (
-        <footer></footer>
+        <footer className="Footer">
+            <div className="LeftFooterColumn">
+                <img className="KryptoLogo" src={ kryptoLogo } alt="The Brand logo, all in uppercase and in white bold lettering"></img>
+            </div>
+            <div className="MiddleFooterColumn">
+                <ul className="FooterMenu">
+                    <li className="FooterMenuTitle">krypto</li>
+                    <li className="FooterItem-1">home</li>
+                    <li className="FooterItem-2">about</li>
+                    <li className="FooterItem-3">buy nfts</li>
+                    <li className="FooterItem-4">sell nfts</li>
+                </ul>
+                <ul className="FooterMenu">
+                    <li className="FooterMenuTitle">market</li>
+                    <li className="FooterItem-1">browse nfts</li>
+                    <li className="FooterItem-2">buy nfts</li>
+                    <li className="FooterItem-3">sell nfts</li>
+                </ul>
+                <ul className="FooterMenu">
+                    <li className="FooterMenuTitle">contact</li>
+                    <li className="FooterItem-1">email</li>
+                    <li className="FooterItem-2">linkedin</li>
+                    <li className="FooterItem-3">instagram</li>
+                    <li className="FooterItem-3">twitter</li>
+                </ul>
+            </div>
+            <div className="RightFooterColumn NewsletterSignUp">
+                <p>join our newsletter</p>
+                <input className="NewsletterSignUpInput" placeholder="email address"></input>
+                <button className="NewsletterSignUpButton">submit</button>
+            </div>
+        </footer>
     )
 };
 
